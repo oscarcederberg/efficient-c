92021c810d7d87b545a0c609f6f78686787364a2
oscarcederberg <os7138ce-s@student.lu.se>
2021-12-16 13:33:09 +0100

    Poly

--- poly/poly.c
+++ poly/poly.c
@@ -22,36 +22,41 @@ poly_t*	new_poly_from_string(const char* string){
     poly->coefs = malloc(sizeof(int) * terms);
     poly->expos = malloc(sizeof(int) * terms);
 
-    current_char = *string;
+    current_char = *(string++);
     while(1){
         coef = 1;
         expo = 0;
+        
+        // check sign
         if(current_char == '-'){
             coef = -1;
-            string++; //skip space
+            string++; // skip space
             current_char = *(string++);
         }else if(current_char == '+'){
-            string++; //skip space
+            string++; // skip space
             current_char = *(string++);
         }
 
+        // check coef
         if(isdigit(current_char)){
-            coef *= current_char-'0';
+            coef *= (current_char - '0');
             current_char = *(string++);
             while(isdigit(current_char)){
-                coef += coef * 10 + current_char - '0';
+                coef = coef * 10 + (current_char - '0');
                 current_char = *(string++);
             }
         }
+
+        // check expo
         if(current_char == 'x'){
             expo = 1;
             current_char = *(string++);
             if(current_char == '^'){
                 current_char = *(string++);
-                expo = current_char-'0';
+                expo = (current_char - '0');
                 current_char = *(string++);
                 while(isdigit(current_char)){
-                    expo += expo * 10 + current_char - '0';
+                    expo = expo * 10 + (current_char - '0');
                     current_char = *(string++);
                 }
             }
@@ -68,7 +73,6 @@ poly_t*	new_poly_from_string(const char* string){
         poly->coefs = realloc(poly->coefs, sizeof(int) * terms);
         poly->expos = realloc(poly->expos, sizeof(int) * terms);
 
-        string++; //skip space
         current_char = *(string++);
     }
 
@@ -84,9 +88,7 @@ void free_poly(poly_t* p){
 }
 
 poly_t*	mul(poly_t* p1, poly_t* p2){
-    int max_terms = p1->terms * p2->terms;
-    int* coefs = calloc(max_terms, sizeof(int));
-    int* expos = calloc(max_terms, sizeof(int));
+    int x, y, i = 0;
 
     int  p1_terms = p1->terms;
     int* p1_coefs = p1->coefs;
@@ -94,13 +96,18 @@ poly_t*	mul(poly_t* p1, poly_t* p2){
     int  p2_terms = p2->terms;
     int* p2_coefs = p2->coefs;
     int* p2_expos = p2->expos;
+    
+    int terms_max = p1_terms * p2_terms;
+
+    int* coefs = (int*)calloc(terms_max, sizeof(int));
+    int* expos = (int*)calloc(terms_max, sizeof(int));
 
     poly_t* product = malloc(sizeof(poly_t));
     product->terms = 0;
-    product->coefs = calloc(max_terms, sizeof(int));
-    product->expos = calloc(max_terms, sizeof(int));
-    
-    int x, y, i = 0;
+    product->coefs = (int*)calloc(terms_max, sizeof(int));
+    product->expos = (int*)calloc(terms_max, sizeof(int));
+
+    // multiply together terms
     for (x = 0; x < p1_terms; ++x)
     {
         int p1_coef = p1_coefs[x];
@@ -113,54 +120,52 @@ poly_t*	mul(poly_t* p1, poly_t* p2){
         }
     }
 
-    int coef, expo, terms = 0;
-    for (x = 0; x < max_terms; x++)
-    {
+    // add together terms with same exponent
+    int coef, expo, terms_after_add = 0;
+    for (x = 0; x < terms_max; ++x){ 
         coef = product->coefs[x];
-        expo = product->expos[x];   
-        for (y = 0; y < x + 1; y++)
-        {
-            if(y == x){
-                terms++;
+        expo = product->expos[x];
+        
+        // populate coefs and expos
+        for (y = 0; y < terms_after_add + 1; ++y){
+            if (y == terms_after_add){
+                coefs[y] = coef;
+            } else if (expos[y] == expo){
                 coefs[y] = coef;
                 expos[y] = expo;
-                break;
-            }
-            if(expos[y] == expo){
-                coefs[y] += coef;
-                break;
+                terms_after_add++;
             }
         }
     }
 
-    //bubble sort with two arrays
+    // bubble sort with two arrays
     int largest_expo, last_expo = __INT_MAX__;
-    int term = -1;
-    for (size_t x = 0; x < terms; x++)
-    {
+    int terms_after_sort = 0;
+    for (x = 0; x < terms_after_add; ++x){
         i = 0;    
         largest_expo = 0;
-        for (y = 0; y < terms; y++)
+        for (y = 0; y < terms_after_add; ++y)
         {
             expo = expos[y];
-            if(expo > largest_expo && expo < last_expo){
+            if(expo >= largest_expo && expo < last_expo){
                 largest_expo = expo;
                 i = y;
             }
         }
 
+        // remove term if coef is 0
         coef = coefs[i];
         if(coef != 0){
-            term++;
-            product->coefs[term] = coef;
-            product->expos[term] = largest_expo;
+            product->coefs[terms_after_sort] = coef;
+            product->expos[terms_after_sort] = largest_expo;
+            terms_after_sort++;
         }
         last_expo = largest_expo;
     }
 
-    product->terms = term;
-    product->coefs = realloc(product->coefs, sizeof(int) * term);
-    product->expos = realloc(product->expos, sizeof(int) * term);
+    product->terms = terms_after_sort;
+    product->coefs = realloc(product->coefs, sizeof(int) * terms_after_sort);
+    product->expos = realloc(product->expos, sizeof(int) * terms_after_sort);
 
     free(coefs);
     free(expos);
@@ -174,6 +179,8 @@ void print_poly(poly_t* p){
     {
         coef = p->coefs[i];
         expo = p->expos[i];
+
+        //printf("%dx^%d", coef, expo);       
         
         if(coef < 0){
             printf("- ");
@@ -182,9 +189,10 @@ void print_poly(poly_t* p){
             printf("+ ");
         }
 
-        if(coef != 0) {
+        if(coef != 1 || expo == 0) {
             printf("%d", coef);
         }
+
         if(expo > 0){
             printf("x");
         }
